7d85ffcd504120591a5efdb0b9e2e8c601488fd1
Cristian Moreno <cristianfmoreno95@gmail.com>
2025-02-10 17:08:19 -0500

    serializer added, views added. Templates deleted because are using restframework

--- core/serializer.py
+++ core/serializer.py
@@ -2,21 +2,33 @@ from rest_framework import serializers
 from .models import User, Student, Teacher, Class, Recording, ClassResume, Task, Review, Specialization
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    User serializer
+    """
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'role']
 
+
 class StudentSerializer(serializers.ModelSerializer):
+    """
+    Student serializer
+    """
     user = UserSerializer()
+
 
     class Meta:
         model = Student
-        fields = ['id', 'user', 'name', 'surname', 'patronymic', 'birth_date', 'phone', 'photo']
+        fields = '__all__'
 
     def update(self, instance, validated_data):
+        """
+        Update the student data
+        """
         # Update the user data
         user_data = validated_data.pop('user', {})
         for attr, value in user_data.items():
+
             setattr(instance.user, attr, value)
         instance.user.save()
 
@@ -26,3 +38,37 @@ class StudentSerializer(serializers.ModelSerializer):
         instance.save()
 
         return instance
+    
+    # def create(self, validated_data):
+    #     user_data = validated_data.pop('user', {})
+    #     user = User.objects.create(**user_data)
+    #     student = Student.objects.create(user=user, **validated_data)
+    #     return student
+    
+class TeacherSerializer(serializers.ModelSerializer):
+    """
+    Teacher serializer
+    """
+    user = UserSerializer()
+
+
+    class Meta:
+        model = Teacher
+        fields = '__all__'
+
+    def update(self, instance, validated_data):
+        """
+        Update the teacher data
+        """
+        user_data = validated_data.pop('user, {}')
+        for attr, value in user_data.items():
+            setattr(instance.user, attr, value)
+
+        instance.user.save()
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+
+        return instance
+    
--- core/views.py
+++ core/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
+from .models import Student, Teacher
+from .serializer import StudentSerializer, TeacherSerializer
 
+# rest framework imports
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 # Create your views here.
-def profile(request):
-    return render(request, 'profile.html')
 
-def edit_profile(request):
-    return render(request, 'edit_profile.html')
+@api_view(['GET', 'POST'])
+@permission_classes([IsAuthenticated])
+def update_profile(request):
+    user = request.user
 
+    if user.role == 'student':
+        student = Student.objects.get(user=user)
+        serializer = StudentSerializer(student, data=request.data)
+
+    elif user.role == 'teacher':
+        teacher = Teacher.objects.get(user=user)
+        serializer = TeacherSerializer(teacher, data=request.data)
+    else:
+        return Response({'error': 'Invalid user role'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
